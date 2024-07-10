c36c2f24d6114771e3a6b41d024446652d82e058
kirandhatt <169182085+kirandhatt@users.noreply.github.com>
2024-07-10 14:26:25 -0700

    create webpack, basic options html, fix folder structures

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        content: './content.js',
+        options: './options.js',
+        popup: './popup.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+};
